3c9759e9eebeaf371ccd0f07991e7ef15264e1f1
Bosi Manurung <94754059+bosimanurung@users.noreply.github.com>
2022-11-24 23:35:04 +0700

    Update transform4_phone.py

--- transform4_phone.py
+++ transform4_phone.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
-bsdf = pd.read_csv('c:/xampp/htdocs/python/bsTestETLDQLabLagi/dqthon-participants.csv')
+bsdf = pd.read_csv('c:/xampp/htdocs/python/bsTestETLDQLabLagi/bs-participants.csv')
 
 bsdf['cleaned_phone_number'] = bsdf['phone_number'].str.replace(r'\+62|62', '0')
 
 bsdf['cleaned_phone_number'] = bsdf['cleaned_phone_number'].str.replace(r'[()-]', '')
 
 bsdf['cleaned_phone_number'] = bsdf['cleaned_phone_number'].str.replace(r'\s+', '')
-bsdf.to_csv('c:/xampp/htdocs/python/bsTestETLDQLabLagi/dqthon-participants.csv', index=False)
+bsdf.to_csv('c:/xampp/htdocs/python/bsTestETLDQLabLagi/bs-participants.csv', index=False)
 
 print(bsdf.head(20))
